72a656da4c040b4ef04e53a89eebbea27df41536
Will <willygomezr1@gmail.com>
2023-12-23 11:49:10 -0500

    changes

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const db = require('./db')
-//const cors = require('cors') if the server is hosting the client side we dont use cors
+const cors = require('cors')
 const path = require('path')
 
 const app = express();
@@ -15,7 +15,7 @@ const clientPath = path.resolve(__dirname, '../client/dist')
 //Create a new route that serves up the static files in your client folder
 app.use(express.static(clientPath))
 
-//app.use(cors())
+app.use(cors())
 
 app.use(express.json());
 
@@ -26,9 +26,9 @@ app.get('/', (req, res) => {
 
     //this command allow us to start the server node server/index.js
 
- //app.listen(PORT, () => {
-   //  console.log(`Server is live on port ${PORT}`)
-     //   })
+app.listen(PORT, () => {
+  console.log(`Server is live on port ${PORT}`)
+    })
         
 //CRUD APPLICATION
 app.get('/api/links', db.getLinks);
